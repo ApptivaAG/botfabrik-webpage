7a1d9dafb53ed4cced1f46a852b2fb484c38147f
Philip Schönholzer <phi.sch@hotmail.ch>
2020-02-08 17:21:26 +0100

    Added chatbot back

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,14 +1,14 @@
-// const React = require('react')
+const React = require('react')
 
-// exports.onRenderBody = ({ setPostBodyComponents }) => {
-//   setPostBodyComponents([
-//     <script
-//       id="chatbot"
-//       key="chatbot"
-//       type="text/javascript"
-//       data-server="https://pit.botfabrik.ch/webclient"
-//       defer
-//       src="https://pit.botfabrik.ch/webclient/embed/bundle.js"
-//     />,
-//   ])
-// }
+exports.onRenderBody = ({ setPostBodyComponents }) => {
+  setPostBodyComponents([
+    <script
+      id="chatbot"
+      key="chatbot"
+      type="text/javascript"
+      data-server="https://pit.botfabrik.ch/webclient"
+      defer
+      src="https://pit.botfabrik.ch/webclient/embed/bundle.js"
+    />,
+  ])
+}
