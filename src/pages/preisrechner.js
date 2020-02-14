4d2e2a790c7ea0a785d18a76e553b5d2fd777cb9
Markus Tanner <markus.tanner@apptiva.ch>
2020-02-14 14:52:57 +0100

    preisrechner formular

--- src/pages/preisrechner.js
+++ src/pages/preisrechner.js
@@ -1,9 +1,9 @@
+/* eslint-disable no-alert */
 import React from 'react'
 import styled, { css } from 'styled-components'
 import Layout from '../components/Layout'
-import { Container, Section, Cols } from '../styles'
+import { Container, Section, Cols, Button } from '../styles'
 import Seo from '../components/Seo'
-import ContactFormPreisrechner from '../components/ContactFormPreisrechner'
 import checkmark from '../img/checkmark.svg'
 
 const Price = styled.p`
@@ -27,7 +27,7 @@ const Feature = styled.p`
   margin-block-end: 0.5em;
 `
 
-const Input = styled.input`
+const FeatureInput = styled.input`
   -webkit-appearance: none;
   -moz-appearance: none;
   -o-appearance: none;
@@ -36,19 +36,31 @@ const Input = styled.input`
     checked
       ? checked &&
         css`
-     {
-        content: url("${checkmark}");
-     }`
+            content: url("${checkmark}");
+        `
       : css`
-           {
-            width: 0.3em !important;
-            height: 0.3em !important;
-            outline: 2px solid #009fdc;
-            box-shadow: none;
-            font-size: 2em;
-          }
+          width: 0.3em !important;
+          height: 0.3em !important;
+          outline: 2px solid #009fdc;
+          box-shadow: none;
+          font-size: 2em;
         `}
 `
+
+const Input = styled.input`
+  display: block;
+  width: 100%;
+  padding: 0.5em;
+  margin: 0.5em 0 1em 0;
+  outline: none;
+  color: #777777;
+  background: #fff;
+  border: 1px solid #e8e8e8;
+  border-radius: 2px;
+  box-sizing: border-box;
+`
+// eslint-disable-next-line react/jsx-props-no-spreading
+const Textarea = props => <Input as="textarea" {...props} />
 
 const BASE_PRICE = 199
 
@@ -111,11 +123,18 @@ const additionalFeatures = [
   },
 ]
 
+const encode = data =>
+  Object.keys(data)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
+    .join('&')
+
 class Preisrechner extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { price: BASE_PRICE }
+    this.state = { price: BASE_PRICE, name: '', email: '', message: '' }
     this.handleClick = this.handleClick.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   handleClick(e) {
@@ -130,8 +149,49 @@ class Preisrechner extends React.Component {
       )
   }
 
+  handleChange(e) {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  /* Here’s the juicy bit for posting the form submission */
+  handleSubmit(e) {
+    const { email, name } = this.state
+    if (email === '' || name === '') {
+      // eslint-disable-next-line no-undef
+      alert('Ups, ein zwingendes Feld ist noch nicht ausgefüllt.')
+      // eslint-disable-next-line react/destructuring-assignment
+    } else if (this.state['bot-field'] === undefined) {
+      const body = encode({
+        'form-name': 'contact-preisrechner',
+        subject: 'Kontaktformular Preisrechner botfabrik.ch',
+        ...this.state,
+      })
+      // eslint-disable-next-line no-undef
+      fetch('/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body,
+      })
+        .then(() => {
+          // eslint-disable-next-line no-undef
+          alert(
+            'Danke! Wir haben ihre Nachricht erhalten und melden uns so bald wie möglich bei ihnen.'
+          )
+          this.setState({ name: '', email: '', message: '' })
+        })
+        .catch(error => {
+          console.error('Error', error)
+          // eslint-disable-next-line no-undef
+          alert(
+            `Leider hat dies nicht funktioniert. Entschuldigen sie die Umstände. Wenn Sie uns auf info@apptiva.ch ein Email schicken, melden wir uns sofort bei ihnen.`
+          )
+        })
+    }
+    e.preventDefault()
+  }
+
   render() {
-    const { price } = this.state
+    const { price, name, email, message } = this.state
     return (
       <Layout callToAction={false}>
         <Seo title="Preisrechner" description="Wie viel kostet ein Bot?" />
@@ -143,46 +203,103 @@ class Preisrechner extends React.Component {
               Hier können sie herausfinden, wie viel ein Chatbot mit den von
               ihnen gewünschten Funktionen pro Monat kostet.
             </p>
-            <Cols minWidth="20em">
-              <div>
-                <h2>Basispaket</h2>
-                <FieldSet>
-                  {basicFeatures.map(f => (
-                    <Feature key={f}>
-                      <label htmlFor={f}>
-                        <Input checked readOnly type="checkbox" name={f} />
-                        &nbsp; {f}
-                      </label>
-                    </Feature>
-                  ))}
-                </FieldSet>
-              </div>
-              <div>
-                <h2>Zusätzliche Funktionen</h2>
-                <FieldSet>
-                  {additionalFeatures.map(f => (
-                    <Feature key={f.name}>
-                      <label htmlFor={f.name}>
-                        <Input
-                          // eslint-disable-next-line react/destructuring-assignment
-                          checked={this.state[f.name]}
-                          id={f.name}
-                          type="checkbox"
-                          name={f.name}
-                          onClick={this.handleClick}
-                        />
-                        &nbsp; {f.displayName}
-                      </label>
-                    </Feature>
-                  ))}
-                </FieldSet>
-              </div>
-            </Cols>
-            <Price>CHF {price}</Price>
-            <PriceInfo>pro Monat</PriceInfo>
-            <br />
-            <br />
-            <ContactFormPreisrechner />
+            <form
+              name="contact-preisrechner"
+              data-netlify="true"
+              data-netlify-honeypot="bot-field"
+              onSubmit={this.handleSubmit}
+            >
+              <Cols minWidth="20em">
+                <div>
+                  <h2>Basispaket</h2>
+                  <FieldSet>
+                    {basicFeatures.map(f => (
+                      <Feature key={f}>
+                        <label htmlFor={f}>
+                          <FeatureInput
+                            checked
+                            readOnly
+                            type="checkbox"
+                            name={f}
+                          />
+                          &nbsp; {f}
+                        </label>
+                      </Feature>
+                    ))}
+                  </FieldSet>
+                </div>
+                <div>
+                  <h2>Zusätzliche Funktionen</h2>
+                  <FieldSet>
+                    {additionalFeatures.map(f => (
+                      <Feature key={f.name}>
+                        <label htmlFor={f.name}>
+                          <FeatureInput
+                            // eslint-disable-next-line react/destructuring-assignment
+                            checked={this.state[f.name]}
+                            id={f.name}
+                            type="checkbox"
+                            name={f.name}
+                            onClick={this.handleClick}
+                          />
+                          &nbsp; {f.displayName}
+                        </label>
+                      </Feature>
+                    ))}
+                  </FieldSet>
+                </div>
+              </Cols>
+              <Price>CHF {price}</Price>
+              <PriceInfo>pro Monat</PriceInfo>
+              <br />
+              <br />
+              <p hidden>
+                <label htmlFor="bot-field">
+                  Nicht ausfüllen:{' '}
+                  <input
+                    type="text"
+                    name="bot-field"
+                    onChange={this.handleChange}
+                  />
+                </label>
+                <input type="text" name="subject" />
+              </p>
+              <p>
+                <label htmlFor="name">
+                  Ihr Name (Pflichtfeld){' '}
+                  <Input
+                    type="text"
+                    name="name"
+                    value={name}
+                    onChange={this.handleChange}
+                  />
+                </label>
+              </p>
+              <p>
+                <label htmlFor="email">
+                  Ihre Email-Adresse (Pflichtfeld){' '}
+                  <Input
+                    type="email"
+                    name="email"
+                    value={email}
+                    onChange={this.handleChange}
+                  />
+                </label>
+              </p>
+              <p>
+                <label htmlFor="message">
+                  Ihre Nachricht{' '}
+                  <Textarea
+                    name="message"
+                    value={message}
+                    onChange={this.handleChange}
+                  />
+                </label>
+              </p>
+              <p css="text-align: right;">
+                <Button type="submit">Jetzt Offerte einholen</Button>
+              </p>
+            </form>
           </Container>
         </Section>
       </Layout>
