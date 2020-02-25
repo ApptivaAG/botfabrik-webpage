d99d90b85ab8e91ae16de0dd7782c507cbdcfd42
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2020-02-25 10:05:35 +0100

    No jank on lazy load on mobile

--- src/styles/Cols.js
+++ src/styles/Cols.js
@@ -2,10 +2,10 @@ import styled from 'styled-components'
 
 const Cols = styled.div`
   display: grid;
-  grid-auto-rows: 1fr;
   gap: 1em;
 
   @media (min-width: calc(${p => p.minWidth || '240px'} + 40px)) {
+    grid-auto-rows: 1fr;
     grid-template-columns: repeat(
       auto-fill,
       minmax(${p => p.minWidth || '240px'}, 1fr)
