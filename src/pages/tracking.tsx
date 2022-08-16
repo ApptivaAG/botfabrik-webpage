b93b6c1ea993e4c241135916b5d73bdfb601e4fa
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2022-08-16 13:41:05 +0200

    Added page to exclude tracking

---
+++ src/pages/tracking.tsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from 'react'
+import Layout from '../components/Layout'
+import { Container } from '../styles'
+
+const useLocalStorage = <T,>(
+  storageKey: string,
+  fallbackState: T
+): [T, (t: T) => void] => {
+  const storedValue = localStorage.getItem(storageKey)
+  const [value, setValue] = useState<T>(
+    storedValue ? JSON.parse(storedValue) : fallbackState
+  )
+
+  useEffect(() => {
+    localStorage.setItem(storageKey, JSON.stringify(value))
+  }, [value, storageKey])
+
+  return [value, setValue]
+}
+
+const Tracking = () => {
+  const [ignore, setIgnore] = useLocalStorage('plausible_ignore', false)
+
+  return (
+    <Layout>
+      <Container>
+        <h1>Analytics Exclude</h1>
+        <p>
+          Click the button below to toggle your exclusion in analytics for this
+          site
+        </p>
+        {ignore ? (
+          <>
+            <p>
+              You currently <b>are excluding</b> your visits.
+            </p>
+            <button onClick={() => setIgnore(false)}>
+              Stop excluding my visits
+            </button>
+          </>
+        ) : (
+          <>
+            <p>
+              You currently <b>are not excluding</b> your visits.
+            </p>
+            <button onClick={() => setIgnore(true)}>Exclude my visits</button>
+          </>
+        )}
+      </Container>
+    </Layout>
+  )
+}
+
+export default Tracking
